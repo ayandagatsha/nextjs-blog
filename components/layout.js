c9a7396b6ee3c4617cb3f1ba3e164b4f32fbca45
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-07-12 07:37:13 +0200

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Graphic Designer'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
@@ -30,9 +30,9 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile.png"
               className={utilStyles.borderCircle}
-              height={144}
+              height={200}
               width={144}
               alt={name}
             />
